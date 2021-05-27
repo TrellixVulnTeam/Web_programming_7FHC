1bb16817a237913086ed98edb962da7c2bb6350d
arbrant <art.gv800@gmail.com>
2021-05-27 06:02:55 +0300

    lab5-6

--- gulpfile.js
+++ gulpfile.js
@@ -121,6 +121,15 @@ function js() {
           .pipe(dest("build/"));          // Переміщуємо у папку build/
 }
 
+// Обробляємо txt файли
+function txt() {
+    return src("app/data/**/*.txt",       // Беремо файли з розширенням txt із папки app/data/
+               { base: "app" })           // Задаємо параметр base, щоб зберегти вложеність файлів
+          .pipe(newer("build/"))          // Відфільтровуємо лише змінені файли
+          .pipe(gulp_if(log, debug(opt))) // Відображаємо список оброблюваних файлів
+          .pipe(dest("build/"));          // Переміщуємо у папку build/
+}
+
 // Обробляємо файли зображень
 function img() {
     return src("app/img/**/*.{png,jpg,jpeg,gif}", // Беремо файли з розширеннями png, jpg, jpeg, gif
